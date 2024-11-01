ed2d95bee8abd5d7fd788d381cf4e01910bca6fc
den4ik2975 <den41k2975@yandex.ru>
2024-11-01 13:07:45 +0300

    feat: added lab2

--- Labs/Lab2/src/monitor_threads.sh
+++ Labs/Lab2/src/monitor_threads.sh
@@ -6,16 +6,16 @@
 g++ -pthread main.cpp -o bitonic_sort
 
 # Запускаем программу в фоне
-./bitonic_sort 4194304 2 &
+./bitonic_sort 2048 4 &
 PROG_PID=$!
 
 echo "Program PID: $PROG_PID"
 echo "Monitoring threads..."
 
-# Мониторим каждую секунду
+# Мониторим каждую сотую секунды
 while kill -0 $PROG_PID 2>/dev/null; do
-    echo "$(date +%T) - Number of threads: $(ls /proc/$PROG_PID/task | wc -l)"
-    sleep 0.01
+    echo "$(date +%T%3N) - Number of threads: $(ls /proc/$PROG_PID/task | wc -l)"
+    sleep 0.05
 done
 
 wait $PROG_PID
